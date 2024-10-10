55518fadb0860ac0646c83d8e1f5e4247fe91308
Evelien-Vissers <vissersevelien@gmail.com>
2024-10-10 07:51:58 +0200

    Aanpassingen Header en Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,24 +1,16 @@
-import './Button.css'
+import './Button.css';
+import { Link } from 'react-router-dom';
 
 const Button = ({ text, onClick, type = 'default', size = 'medium', link = '#' }) => {
     return (
-        <a href={link} className={`btn ${type} ${size}`} onClick={onClick} >
+        <Link to={link} className={`btn ${type} ${size}`} onClick={onClick}>
             {text}
-        </a>
+        </Link>
     );
 };
+
 export default Button;
 
-/* Zie hieronder hoe de verschillende buttons gebruikt kunnen worden: */
-/* Login Button: <Button text="Login" type="mint" size="medium" /> */
-/* Start Your Heal Force Journey Now Button: <Button text="Start Your Heal Force Journey Now" type="black" size="large" link="/registration" /> */
-/* Submit Contact Form Button: <Button text="Submit" type="mint" size="medium" /> */
-/* Register Button: <Button text="Register" type="mint" size="medium" /> */
-/* Submit Questionnaire: <Button text="Submit Questionnaire" type="black" size="large" link="/create-profile" /> */
-/* Upload Button <Button text="Upload" type="mint" size="medium" /> */
-/* Cancel Button: <Button text="Cancel" type="cancel" size="medium" /> */
-/* Create My Heal Force Profile Button: <Button text="Create My Heal Force Profile" type="black" size="large" link="/profile" /> */
-/* Start Matching Button: <Button text="Start Matching" type="gradient" size="large" link="/match" /> */
 
 
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import './Header.css'
 import logo from '../../assets/logo.png'
 import {Link} from "react-router-dom";
+import Button from "../button/Button.jsx";
 
 const Header = () => {
     return (
@@ -14,13 +15,14 @@ const Header = () => {
                         <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/mission">Our Mission & Vision</Link>
+                        <Link to="/mission">Our Mission</Link>
                     </li>
                     <li>
                         <Link to="/contact">Contact Us</Link>
                     </li>
                 </ul>
             </nav>
+            <Button text="Login" type="mint" size = "medium" link="/login"/>
         </header>
     )
 }
